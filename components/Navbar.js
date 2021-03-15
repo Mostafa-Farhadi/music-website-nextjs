161bd1e7a19d1156e386f8ebfa8919f9b15ee8e6
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-03-15 21:19:57 +0330

    remove 'a' elements

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,37 +8,37 @@ const Navbar = () => {
 
             <h3>BROWSER MUSIC</h3>
             <img className="icons" src="img/navbar images/home.png" alt="menu"/>
-            <Link href="/"><a>Home</a></Link>
+            <Link href="/">Home</Link>
             <img className="icons" src="img/navbar images/genres.png" alt="menu"/>
-            <Link href="/Genres"><a>Genres</a></Link>
+            <Link href="/Genres">Genres</Link>
             <img className="icons" src="img/navbar images/free music.png" alt="menu"/>
-            <Link href="/FreeMusic"><a>Free Music</a></Link>
+            <Link href="/FreeMusic">Free Music</Link>
             <img className="icons" src="img/navbar images/artist.png" alt="menu"/>
-            <Link href="/Artist"><a>Artist</a></Link>
+            <Link href="/Artist">Artist</Link>
             <img className="icons" src="img/navbar images/Stations.png" alt="menu"/>
-            <Link href="/Stations"><a>Stations</a></Link>
+            <Link href="/Stations">Stations</Link>
 
             <h3>YOUR MUSIC</h3>
             <img className="icons" src="img/navbar images/analytics.png" alt="menu"/>
-            <Link href="/Analytics"><a>Analytics</a></Link>
+            <Link href="/Analytics">Analytics</Link>
             <img className="icons" src="img/navbar images/favorites.png" alt="menu"/>
-            <Link href="/Favorites"><a>Favorites</a></Link>
+            <Link href="/Favorites">Favorites</Link>
             <img className="icons" src="img/navbar images/history.png" alt="menu"/>
-            <Link href="/History"><a>History</a></Link>
+            <Link href="/History">History</Link>
 
             <h3>MUSIC EVENTS</h3>
             <img className="icons" src="img/navbar images/events.png" alt="menu"/>
-            <Link href="/Events"><a>Events</a></Link>
+            <Link href="/Events">Events</Link>
             <img className="icons" src="img/navbar images/event details.png" alt="menu"/>
-            <Link href="/EventDetails"><a>Event Details</a></Link>
+            <Link href="/EventDetails">Event Details</Link>
             <img className="icons" src="img/navbar images/add events.png" alt="menu"/>
-            <Link href="/AddEvent"><a>Add Event</a></Link>
+            <Link href="/AddEvent">Add Event</Link>
 
             <h3>EXTRA AGES</h3>
             <img className="icons" src="img/navbar images/error.png" alt="menu"/>
-            <Link href="/Error"><a>Error</a></Link>
+            <Link href="/Error">Error</Link>
             <img className="icons" src="img/navbar images/blank.png" alt="menu"/>
-            <Link href="/Blank"><a>Blank</a></Link>
+            <Link href="/Blank">Blank</Link>
             
         </nav>
     )
