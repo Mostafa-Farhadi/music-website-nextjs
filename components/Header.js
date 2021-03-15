43c2434014218a1471cbaf3b30e901b5584f92cf
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-03-15 12:39:12 +0330

    add icons to navbar

--- components/Header.js
+++ components/Header.js
@@ -2,11 +2,11 @@ const Header = () => {
     return ( 
         <div id="header" >
             <form action="">
-                <img src="/img/search.png" alt=""/>
+                <img src="/img/header/search.png" alt=""/>
                 <input type="search" placeholder="Search"></input>
             </form>
             <p>Language</p>
-            <img src="/img/avatar.png" alt="avatar"/>
+            <img src="/img/header/avatar.png" alt="avatar"/>
             <p>Admin</p>
         </div>
     );
--- components/Navbar.js
+++ components/Navbar.js
@@ -3,41 +3,41 @@ import Link from 'next/link'
 const Navbar = () => {
     return (
         <nav id="navbar">
-            <Link href="/"><img className="logo" src="/img/musicLogo.png" alt="music logo"/></Link>
-            <img className="menu-icon" src="img/menu.png" alt="menu"/>
+            <Link href="/"><img className="logo" src="/img/navbar images/musicLogo.png" alt="music logo"/></Link>
+            <img className="menu-icon" src="/img/navbar images/menu.png" alt="menu"/>
 
             <h3>BROWSER MUSIC</h3>
-            <img className="icons" src="img/menu.png" alt="menu"/>
+            <img className="icons" src="img/navbar images/home.png" alt="menu"/>
             <Link href="/"><a>Home</a></Link>
-            <img className="icons" src="img/menu.png" alt="menu"/>
+            <img className="icons" src="img/navbar images/genres.png" alt="menu"/>
             <Link href="/Genres"><a>Genres</a></Link>
-            <img className="icons" src="img/menu.png" alt="menu"/>
+            <img className="icons" src="img/navbar images/free music.png" alt="menu"/>
             <Link href="/FreeMusic"><a>Free Music</a></Link>
-            <img className="icons" src="img/menu.png" alt="menu"/>
+            <img className="icons" src="img/navbar images/artist.png" alt="menu"/>
             <Link href="/Artist"><a>Artist</a></Link>
-            <img className="icons" src="img/menu.png" alt="menu"/>
+            <img className="icons" src="img/navbar images/station.png" alt="menu"/>
             <Link href="/Station"><a>Station</a></Link>
 
             <h3>YOUR MUSIC</h3>
-            <img className="icons" src="img/menu.png" alt="menu"/>
+            <img className="icons" src="img/navbar images/analytics.png" alt="menu"/>
             <Link href="/Analytics"><a>Analytics</a></Link>
-            <img className="icons" src="img/menu.png" alt="menu"/>
+            <img className="icons" src="img/navbar images/favorites.png" alt="menu"/>
             <Link href="/Favorites"><a>Favorites</a></Link>
-            <img className="icons" src="img/menu.png" alt="menu"/>
+            <img className="icons" src="img/navbar images/history.png" alt="menu"/>
             <Link href="/History"><a>History</a></Link>
 
             <h3>MUSIC EVENTS</h3>
-            <img className="icons" src="img/menu.png" alt="menu"/>
+            <img className="icons" src="img/navbar images/events.png" alt="menu"/>
             <Link href="/Events"><a>Events</a></Link>
-            <img className="icons" src="img/menu.png" alt="menu"/>
+            <img className="icons" src="img/navbar images/event details.png" alt="menu"/>
             <Link href="/EventDetails"><a>Event Details</a></Link>
-            <img className="icons" src="img/menu.png" alt="menu"/>
+            <img className="icons" src="img/navbar images/add events.png" alt="menu"/>
             <Link href="/AddEvent"><a>Add Event</a></Link>
 
             <h3>EXTRA AGES</h3>
-            <img className="icons" src="img/menu.png" alt="menu"/>
+            <img className="icons" src="img/navbar images/error.png" alt="menu"/>
             <Link href="/Error"><a>Error</a></Link>
-            <img className="icons" src="img/menu.png" alt="menu"/>
+            <img className="icons" src="img/navbar images/blank.png" alt="menu"/>
             <Link href="/Blank"><a>Blank</a></Link>
             
         </nav>
