7019a9d35ca6feecdf8cfc4898ec45858b7cd8df
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-03-15 11:59:42 +0330

    create MusicBox component for holding Boxes of music in FreeMusic Page

--- pages/FreeMusic.js
+++ pages/FreeMusic.js
@@ -1,49 +1,27 @@
+import MusicBox from '../components/Free Music Page/MusicBox'
+
 const FreeMusic = () => {
     return ( 
         <div id="free-music">
           <div className="head"></div>
 
-          <div className="top-chart">
-            <h2>Top Chart</h2>
-            <p>Listen top chart</p>
-            <hr/>
-            <div className="box-container">
-              <div className="box one">1</div>
-              <div className="box two">2</div>
-              <div className="box three">3</div>
-              <div className="box four">4</div>
-              <div className="box five">5</div>
-              <div className="box six">6</div>
-            </div>
-          </div>
+          <MusicBox
+            nameOfClass = "top-chart"
+            title="Top Chart" 
+            information="Listen top chart" 
+          />
 
-          <div className="new-releases">
-            <h2>New Releases</h2>
-            <p>Listen recently release music</p>
-            <hr/>
-            <div className="box-container">
-              <div className="box one">1</div>
-              <div className="box two">2</div>
-              <div className="box three">3</div>
-              <div className="box four">4</div>
-              <div className="box five">5</div>
-              <div className="box six">6</div>
-            </div>
-          </div>
+          <MusicBox
+            nameOfClass = "new-releases"
+            title="New Releases" 
+            information="Listen recently release music" 
+          />
 
-          <div className="featured-artists">
-            <h2>Featured Artists</h2>
-            <p>Select you best to listen</p>
-            <hr/>
-            <div className="box-container">
-              <div className="box one">1</div>
-              <div className="box two">2</div>
-              <div className="box three">3</div>
-              <div className="box four">4</div>
-              <div className="box five">5</div>
-              <div className="box six">6</div>
-            </div>
-          </div>
+          <MusicBox
+            nameOfClass = "featured-artists"
+            title="Featured Artists" 
+            information="Select you best to listen" 
+          />
 
           <div className="foot"></div>
         </div>
