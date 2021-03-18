1929630d81d2780a3fcac66c200a6950bcaf553b
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-03-18 11:31:35 +0330

    1- Remove cover section and change grid layout
    2- Set a picture for background of page
    
    3- Change picture of artists
    
    add right padding to search img

--- components/index/Cover.js
+++
@@ -1,7 +0,0 @@
-const Artist = () => {
-    return ( 
-        <section className="cover"><h2>cover</h2></section>
-    );
-}
-
-export default Artist;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Cover from '../components/index/Cover'
 import Artist from '../components/index/Artist'
 import Album from '../components/index/Album'
 import Music from '../components/index/Music'
@@ -9,18 +8,14 @@ const Home = () => {
 
   return ( 
     <div id="home">
-
       <Head>
         <title>Music Website | Home</title>
         <meta name="keywords" content="Music"/>
       </Head>
-
-      <Cover />
       <Artist />
       <Album />
       <Music />
       <Player />
-      
     </div>
   );
 }
