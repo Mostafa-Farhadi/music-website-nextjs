8bbd262035c4a828e364a12ebf736e7fafc3d884
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-03-18 11:48:36 +0330

    change name of imported component in index.js file

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
-import Artist from '../components/index/ArtistsSection'
-import Album from '../components/index/AlbumsSection'
-import Music from '../components/index/MusicsSection'
-import Player from '../components/index/PlayerSection'
+import ArtistsSection from '../components/index/ArtistsSection'
+import AlbumsSection from '../components/index/AlbumsSection'
+import MusicsSection from '../components/index/MusicsSection'
+import PlayerSection from '../components/index/PlayerSection'
 
 const Home = () => {
 
@@ -12,10 +12,10 @@ const Home = () => {
         <title>Music Website | Home</title>
         <meta name="keywords" content="Music"/>
       </Head>
-      <Artist />
-      <Album />
-      <Music />
-      <Player />
+      <ArtistsSection />
+      <AlbumsSection />
+      <MusicsSection />
+      <PlayerSection />
     </div>
   );
 }
