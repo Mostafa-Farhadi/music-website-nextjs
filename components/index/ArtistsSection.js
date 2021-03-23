559dd37fc305125281395bbcf3d0b055f093363f
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-03-23 19:09:10 +0430

    add Artists to artists-section and style them

--- components/index/ArtistsSection.js
+++ components/index/ArtistsSection.js
@@ -1,6 +1,31 @@
 const ArtistsSection = () => {
     return ( 
-        <section className="artists-section"><h2>Artists Section</h2></section>
+        <section className="artists-section">
+            <div className="one">
+                <img src="img/cover/billie eilish.jpg" alt="billie eilish"/>
+                <button>Billie Eilish</button>
+            </div>
+            <div className="two">
+                <img src="img/cover/dua lipa.jpg" alt="dua lipa"/>
+                <button>Dua Lipa</button>
+            </div>
+            <div className="three">
+                <img src="img/cover/justin beiber.jpg" alt="justin beiber"/>
+                <button>Justin Beiber</button>
+            </div>
+            <div className="four">
+                <img src="img/cover/pitbull.jpg" alt="pitbull"/>
+                <button>Pitbull</button>
+            </div>
+            <div className="five">
+                <img src="img/cover/selena gomez.jpg" alt="selena gomez"/>
+                <button>Selena Gomez</button>
+            </div>
+            <div className="six">
+                <img src="img/cover/taylor swift.jpg" alt="taylor swift"/>
+                <button>Taylor Swift</button>
+            </div>
+        </section>
     );
 }
 
