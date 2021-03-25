8d194041f70a129acc716c62c5a039454a02ced8
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-03-26 01:43:12 +0430

    1. Delete img element and set artists images as background of div elements
    2. Use p element for adding hovering effect

--- components/index/ArtistsSection.js
+++ components/index/ArtistsSection.js
@@ -1,39 +1,17 @@
 const ArtistsSection = () => {
     return ( 
         <section className="artists-section">
-            <div className="one">
-                <img src="img/cover/billie eilish.jpg" alt="billie eilish"/>
-            </div>
-            <div className="two">
-                <img src="img/cover/dua lipa.jpg" alt="dua lipa"/>
-            </div>
-            <div className="three">
-                <img src="img/cover/justin beiber.jpg" alt="justin beiber"/>
-            </div>
-            <div className="four">
-                <img src="img/cover/pitbull.jpg" alt="pitbull"/>
-            </div>
-            <div className="five">
-                <img src="img/cover/selena gomez.jpg" alt="selena gomez"/>
-            </div>
-            <div className="six">
-                <img src="img/cover/taylor swift.jpg" alt="taylor swift"/>
-            </div>
-            <div className="seven">
-                <img src="img/cover/justin timberlake.jpg" alt="justin timberlake"/>
-            </div>
-            <div className="eight">
-                <img src="img/cover/enrique iglesias.jpg" alt="enrique iglesias"/>
-            </div>
-            <div className="nine">
-                <img src="img/cover/Rihanna.jpg" alt="Rihanna"/>
-            </div>
-            <div className="ten">
-                <img src="img/cover/shakira.jpg" alt="shakira"/>
-            </div>
-            <div className="eleven">
-                <img src="img/cover/bts.jpg" alt="bts"/>
-            </div>
+            <div className="one"><p></p></div>
+            <div className="two"><p></p></div>
+            <div className="three"><p></p></div>
+            <div className="four"><p></p></div>
+            <div className="five"><p></p></div>
+            <div className="six"><p></p></div>
+            <div className="seven"><p></p></div>
+            <div className="eight"><p></p></div>
+            <div className="nine"><p></p></div>
+            <div className="ten"><p></p></div>
+            <div className="eleven"><p></p></div>
         </section>
     );
 }
