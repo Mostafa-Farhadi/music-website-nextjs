cec27d58f14d252d7aea8d545b6daf13fa56b08d
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-03-26 00:57:54 +0430

    Update artists and change their styles

--- components/index/ArtistsSection.js
+++ components/index/ArtistsSection.js
@@ -3,27 +3,36 @@ const ArtistsSection = () => {
         <section className="artists-section">
             <div className="one">
                 <img src="img/cover/billie eilish.jpg" alt="billie eilish"/>
-                <button>Billie Eilish</button>
             </div>
             <div className="two">
                 <img src="img/cover/dua lipa.jpg" alt="dua lipa"/>
-                <button>Dua Lipa</button>
             </div>
             <div className="three">
                 <img src="img/cover/justin beiber.jpg" alt="justin beiber"/>
-                <button>Justin Beiber</button>
             </div>
             <div className="four">
                 <img src="img/cover/pitbull.jpg" alt="pitbull"/>
-                <button>Pitbull</button>
             </div>
             <div className="five">
                 <img src="img/cover/selena gomez.jpg" alt="selena gomez"/>
-                <button>Selena Gomez</button>
             </div>
             <div className="six">
                 <img src="img/cover/taylor swift.jpg" alt="taylor swift"/>
-                <button>Taylor Swift</button>
+            </div>
+            <div className="seven">
+                <img src="img/cover/justin timberlake.jpg" alt="justin timberlake"/>
+            </div>
+            <div className="eight">
+                <img src="img/cover/enrique iglesias.jpg" alt="enrique iglesias"/>
+            </div>
+            <div className="nine">
+                <img src="img/cover/Rihanna.jpg" alt="Rihanna"/>
+            </div>
+            <div className="ten">
+                <img src="img/cover/shakira.jpg" alt="shakira"/>
+            </div>
+            <div className="eleven">
+                <img src="img/cover/bts.jpg" alt="bts"/>
             </div>
         </section>
     );
