3ac824463787f981bf74cddcc11020e93186fa23
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-03-27 12:36:09 +0430

    Use axios to fetching data from api endpoint to update state

--- redux/artist/artistAction.js
+++ redux/artist/artistAction.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { EILISH } from './artistTypes';
 import { LIPA } from './artistTypes';
 import { BEIBER } from './artistTypes';
@@ -10,107 +11,112 @@ import { RIHANNA } from './artistTypes';
 import { SHAKIRA } from './artistTypes';
 import { BTS } from './artistTypes';
 
-export const eilishArtist = () => {
+export const eilishArtist = (name) => {
     return {
         type: EILISH,
-        payload: "Billie Eilish Albumes"
+        payload: name[0]
     }
 }
 
-export const lipaArtist = () => {
+export const lipaArtist = (name) => {
     return {
         type: LIPA,
-        payload: "Dua Lipa Albumes"
+        payload: name[1]
     }
 }
 
-export const beiberArtist = () => {
+export const beiberArtist = (name) => {
     return {
         type: BEIBER,
-        payload: "Justin Beiber Albumes"
+        payload: name[2]
     }
 }
 
-export const pitbullArtist = () => {
+export const pitbullArtist = (name) => {
     return {
         type: PITBULL,
-        payload: "Pitbull Albumes"
+        payload: name[3]
     }
 }
 
-export const gomezArtist = () => {
+export const gomezArtist = (name) => {
     return {
         type: GOMEZ,
-        payload: "Selena Gomez Albumes"
+        payload: name[4]
     }
 }
 
-export const swiftArtist = () => {
+export const swiftArtist = (name) => {
     return {
         type: SWIFT,
-        payload: "Taylor Swift Albumes"
+        payload: name[5]
     }
 }
 
-export const timberlakeArtist = () => {
+export const timberlakeArtist = (name) => {
     return {
         type: TIBERLAKE,
-        payload: "Justin Timberlake Albumes"
+        payload: name[6]
     }
 }
 
-export const iglesiasArtist = () => {
+export const iglesiasArtist = (name) => {
     return {
         type: IGLESIAS,
-        payload: "Enrique Iglesias Albumes"
+        payload: name[7]
     }
 }
 
-export const rihannaArtist = () => {
+export const rihannaArtist = (name) => {
     return {
         type: RIHANNA,
-        payload: "Rihanna Albumes"
+        payload: name[8]
     }
 }
 
-export const shakiraArtist = () => {
+export const shakiraArtist = (name) => {
     return {
         type: SHAKIRA,
-        payload: "Shakira Albumes"
+        payload: name[9]
     }
 }
 
-export const btsArtist = () => {
+export const btsArtist = (name) => {
     return {
         type: BTS,
-        payload: "Bts Group Albumes"
+        payload: name[10]
     }
 }
 
 export const choseArtist = (Artist) => {
     return (dispatch) => {
-        if (Artist === "Eilish") {
-            dispatch(eilishArtist())
-        } else if (Artist === "Lipa") {
-            dispatch(lipaArtist())
-        } else if (Artist === "Beiber") {
-            dispatch(beiberArtist())
-        } else if (Artist === "Pitbull") {
-            dispatch(pitbullArtist())
-        } else if (Artist === "Gomez") {
-            dispatch(gomezArtist())
-        } else if (Artist === "Swift") {
-            dispatch(swiftArtist())
-        } else if (Artist === "Timberlake") {
-            dispatch(timberlakeArtist())
-        } else if (Artist === "Iglesias") {
-            dispatch(iglesiasArtist())
-        } else if (Artist === "Rihanna") {
-            dispatch(rihannaArtist())
-        } else if (Artist === "Shakira") {
-            dispatch(shakiraArtist())
-        } else if (Artist === "Bts") {
-            dispatch(btsArtist())
-        }
+        axios.get("http://localhost:3000/api/musics")
+        .then((response) => {
+            const artist = response.data
+            if (Artist === "Eilish") {
+                dispatch(eilishArtist(artist))
+            } else if (Artist === "Lipa") {
+                dispatch(lipaArtist(artist))
+            } else if (Artist === "Beiber") {
+                dispatch(beiberArtist(artist))
+            } else if (Artist === "Pitbull") {
+                dispatch(pitbullArtist(artist))
+            } else if (Artist === "Gomez") {
+                dispatch(gomezArtist(artist))
+            } else if (Artist === "Swift") {
+                dispatch(swiftArtist(artist))
+            } else if (Artist === "Timberlake") {
+                dispatch(timberlakeArtist(artist))
+            } else if (Artist === "Iglesias") {
+                dispatch(iglesiasArtist(artist))
+            } else if (Artist === "Rihanna") {
+                dispatch(rihannaArtist(artist))
+            } else if (Artist === "Shakira") {
+                dispatch(shakiraArtist(artist))
+            } else if (Artist === "Bts") {
+                dispatch(btsArtist(artist))
+            }
+
+        });
     }
 }
