6be77b798d203d33297064a3e0cbcc3e7d26fffe
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-03-28 12:03:44 +0430

    Revise choseArtist input argument name

--- components/index/ArtistsSection.js
+++ components/index/ArtistsSection.js
@@ -23,7 +23,7 @@ const ArtistsSection = (props) => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        choseArtist: (artist) => dispatch(choseArtist(artist))
+        choseArtist: (artistId) => dispatch(choseArtist(artistId))
     }
 }
 
