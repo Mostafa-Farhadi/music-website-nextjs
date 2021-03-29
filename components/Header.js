362efdd07a433c4235b22e48a4f52974244950f0
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-03-29 19:22:12 +0430

    Change direction of source image

--- components/Header.js
+++ components/Header.js
@@ -2,8 +2,8 @@
 const Header = () => {
     return ( 
         <header id="header" >
-            <img src="img/header/logo.png" alt="logo" className="logo"/>
-            <img src="img/header/search.png" alt="search" className="search"/>
+            <img src="img/icon/logo.png" alt="logo" className="logo"/>
+            <img src="img/icon/search.png" alt="search" className="search"/>
             <input type="search" name="search" id="search" placeholder="Search now..."/>
         </header>
     );
