ac4fa139ae309e437966318238ed5863803c8686
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-03-30 09:50:32 +0430

    Change style of artist section and turn it to neumorphism

--- components/index/ArtistsSection.js
+++ components/index/ArtistsSection.js
@@ -6,17 +6,17 @@ const ArtistsSection = (props) => {
 
     return ( 
         <section className="artists-section">
-            <div className="one" title="Billie Eilish" onClick={() => choseArtist(0)} ><p></p></div>
-            <div className="two" title="Dua Lipa" onClick={() => choseArtist(1)} ><p></p></div>
-            <div className="three" title="Justin Beiber" onClick={() => choseArtist(2)} ><p></p></div>
-            <div className="four" title="Pitbull" onClick={() => choseArtist(3)} ><p></p></div>
-            <div className="five" title="Selena Gomez" onClick={() => choseArtist(4)} ><p></p></div>
-            <div className="six" title="Taylor Swift" onClick={() => choseArtist(5)} ><p></p></div>
-            <div className="seven" title="Justin Timberlake" onClick={() => choseArtist(6)} ><p></p></div>
-            <div className="eight" title="Enrique Iglesias" onClick={() => choseArtist(7)} ><p></p></div>
-            <div className="nine" title="Rihanna" onClick={() => choseArtist(8)} ><p></p></div>
-            <div className="ten" title="Shakira" onClick={() => choseArtist(9)} ><p></p></div>
-            <div className="eleven" title="Bts" onClick={() => choseArtist(10)} ><p></p></div>
+            <div className="artist one" title="Billie Eilish" onClick={() => choseArtist(0)} ></div>
+            <div className="artist two" title="Dua Lipa" onClick={() => choseArtist(1)} ></div>
+            <div className="artist three" title="Justin Beiber" onClick={() => choseArtist(2)} ></div>
+            <div className="artist four" title="Pitbull" onClick={() => choseArtist(3)} ></div>
+            <div className="artist five" title="Selena Gomez" onClick={() => choseArtist(4)} ></div>
+            <div className="artist six" title="Taylor Swift" onClick={() => choseArtist(5)} ></div>
+            <div className="artist seven" title="Justin Timberlake" onClick={() => choseArtist(6)} ></div>
+            <div className="artist eight" title="Enrique Iglesias" onClick={() => choseArtist(7)} ></div>
+            <div className="artist nine" title="Rihanna" onClick={() => choseArtist(8)} ></div>
+            <div className="artist ten" title="Shakira" onClick={() => choseArtist(9)} ></div>
+            <div className="artist eleven" title="Bts" onClick={() => choseArtist(10)} ></div>
         </section>
     );
 }
