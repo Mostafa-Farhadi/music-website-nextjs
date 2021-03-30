cf07487bc677c0833b67002a78745a8ace26d7db
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-03-30 09:48:11 +0430

    Change style of header

--- components/Header.js
+++ components/Header.js
@@ -3,8 +3,10 @@ const Header = () => {
     return ( 
         <header id="header" >
             <img src="img/icon/logo.png" alt="logo" className="logo"/>
-            <img src="img/icon/search.png" alt="search" className="search"/>
-            <input type="search" name="search" id="search" placeholder="Search now..."/>
+            <div className="search-box">
+                <img src="img/icon/search.png" alt="search" className="search"/>
+                <input type="search" name="search" id="search" placeholder="Search now..."/>
+                </div>
         </header>
     );
 }
