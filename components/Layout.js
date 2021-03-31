f9e472ff33cc99190414e4aee3649cee3925c3bc
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-03-31 15:51:09 +0430

    Add Footer component to layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,12 @@
 import Header from './Header'
+import Footer from './Footer'
 
 const Layout = ({ children }) => {
     return (
         <div id="wrapper">
             <Header />
             <div id="content">{ children }</div>
+            <Footer />
         </div>
 
     );
