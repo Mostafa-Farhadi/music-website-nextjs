29dd8c0ef650bac8b35e90314b1336a975922314
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-04-04 12:31:50 +0430

    Add some link of used tools that are used to create this nextjs app and my github link to footer and style them

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,15 @@
 const Footer = () => {
     return ( 
         <footer id="footer">
-            
+                <a href="https://nextjs.org/"><img className="nextjs" src="img/icon/nextjs.svg" alt="nextjs"/></a>
+                <p>See more projects on my github</p>
+                <a href="https://github.com/Mostafa-Farhadi"><img className="logo" src="img/icon/github.png" alt="github"/></a>
+                <p>Some Tools Used</p>
+                <a href="https://html.com/html5/"><img className="logo" src="img/icon/html5.png" alt="html5"/></a>
+                <a href="https://sass-lang.com/"><img className="logo" src="img/icon/sass.png" alt="sass"/></a>
+                <a href="https://www.javascript.com/"><img className="logo" src="img/icon/javascript.png" alt="javascript"/></a>
+                <a href="https://reactjs.org/"><img className="logo" src="img/icon/react.png" alt="react"/></a>
+                <a href="https://react-redux.js.org/"><img className="logo" src="img/icon/redux.png" alt="redux"/></a>
         </footer>
     );
 }
