e62cae8bd79729aa63424b601ac5547ccbdb60c6
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-04-26 22:33:51 +0430

    1. Enable search ability to app
    2. Add new className to form input to hide search box when user is not in music page

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,59 @@
-import Link from 'next/link'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Link from 'next/link';
+import { connect } from "react-redux";
+import {choseArtist} from '../redux/artist/artistAction';
+import { useRef } from 'react';
+import { useRouter } from 'next/router';
 
-const Header = () => {
+const Header = (props) => {
+    const { choseArtist } = props;
+    const [artists, setArtists] = useState([]);
+    const [suggests, setSuggests] = useState([]);
+    const [enteredText, setEnteredText] = useState("");
+    const router = useRouter();
+    const searchBox = useRef(null);
+    const [enabledSearchBox, setEnabledSearchBox] = useState("false");
+
+        useEffect(() => {
+            if (router.pathname !== "/music") {
+                setEnabledSearchBox("disabled-search-box")
+            } else {
+                setEnabledSearchBox("")
+            }
+        }, [router.pathname]);
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/api/musics")
+        .then((result) => {
+            let artistsArr = [];
+            result.data.forEach(artist => {
+                artistsArr.push(artist.name)
+            });
+            setArtists(artistsArr);
+        }).catch((error) => {
+            console.log(error);
+        });
+    }, []);
+
+    const suggestHandler = event => {
+        const text = event.target.value.toLowerCase();
+        if (text !== "") {
+            let suggestionsArr = [];
+            for (let i = 0; i < artists.length; i++) {
+                if (artists[i].slice(0, text.length).toLowerCase() === text) {
+                    suggestionsArr.push([artists[i].replace(artists[i].substr(0, text.length), ""), i]);
+                    setEnteredText(artists[i].slice(0, text.length));
+                } else {
+                    suggestionsArr;
+                }
+            }
+            setSuggests(suggestionsArr);
+        } else {
+            setSuggests([]);
+        }
+    };
+
     return ( 
         <header id="header" >
             <ul>
@@ -15,12 +68,30 @@ const Header = () => {
                     </Link>
                 </li>
             </ul>
-            <div className="search-box">
+            <form className={`search-box ${enabledSearchBox}`} autoComplete="off" ref={searchBox}>
                 <img src="img/icon/search.png" alt="search" className="search-logo"/>
-                <input type="search" name="search" id="search" className="search" placeholder="Search now..."/>
+                <input 
+                    type="search" 
+                    name="search" 
+                    id="search" 
+                    className="search" 
+                    placeholder="Search now..."
+                    onChange={event => suggestHandler(event)}
+                />
+            </form>
+            <div className="suggestions">
+                {suggests.map((suggest, index) => (
+                        <a key={index} onClick={() => choseArtist(suggest[1])}><span>{enteredText}</span>{suggest[0]}</a>
+                ))}
             </div>
         </header>
     );
 }
 
-export default Header;
+const mapDispatchToProps = dispatch => {
+    return {
+        choseArtist: (artistId) => dispatch(choseArtist(artistId))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Header);
